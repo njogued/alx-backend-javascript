e6f8a68f3c95a64285c949b7e541d68ade8f1c09
njogued <njogued@gmail.com>
2023-07-01 19:37:28 +0300

    Using .entries() method

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const [idx, value] of array.entries()) {
+    newArray[idx] = appendString + value;
+  }
+  return newArray;
+}
