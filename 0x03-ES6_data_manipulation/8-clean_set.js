1583af955243c272f6bb1669175dd99a3f457fb3
njogued <njogued@gmail.com>
2023-07-10 02:14:54 +0300

    unique map

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (!startString || !set || startString.length === 0) {
+    return "";
+  }
+  return Array.from(set)
+    .filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join("-");
+}
