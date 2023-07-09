114c0ce0528e5ae2c89791907aced8bca461ed03
njogued <njogued@gmail.com>
2023-07-09 22:47:25 +0300

    idk

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(students) {
   let studentIds = [];
   if (Array.isArray(students)) {
-    studentIds = students.map(({ id }) => id);
+    return students.map(({ id }) => id);
   }
   return studentIds;
 }
