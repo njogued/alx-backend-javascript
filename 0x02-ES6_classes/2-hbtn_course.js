c5e51c47b3db0ce8eb834ee46d61aa4ac1c88c15
njogued <njogued@gmail.com>
2023-07-18 00:45:29 +0300

    HBTN

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === "string") {
+      this._name = name;
+    } else {
+      throw new TypeError("Name must be a string");
+    }
+    if (typeof length === "number") {
+      this._length = length;
+    } else {
+      throw new TypeError("Length must be a number");
+    }
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError("Students must be an Array");
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name === "string") {
+      this._name = name;
+    } else {
+      throw new TypeError("Name must be a string");
+    }
+  }
+
+  set length(length) {
+    if (typeof length === "number") {
+      this._length = length;
+    } else {
+      throw new TypeError("Length must be a number");
+    }
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError("Students must be an Array");
+    }
+  }
+}
