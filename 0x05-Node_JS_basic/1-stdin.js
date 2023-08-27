6d0e0aaec79c47920f7d93d7335b862a33352fb7
njogued <njogued@gmail.com>
2023-08-27 10:50:15 +0300

    Alternative stdin function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-readline.question(
-  "Welcome to Holberton School, what is your name?\n",
-  (name) => {
-    console.log(`Your name is: ${name}`);
-    console.log("This important software is now closing");
-    readline.close();
-  }
-);
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
